53acd15dfd5abcf8f232fd60951afe764993de86
Nayeli De Jesus <nayelidjm142@gmail.com>
2020-05-09 14:22:09 -0700

    Open Cv image

--- opencv.py
+++ opencv.py
@@ -1,26 +1,17 @@
+from PIL import Image
 import numpy as np
 import cv2
 import datetime
-
-x = datetime.datetime.now()
-
-if current_weather >= 5 and current_weather <= 17:
-#daytime
-    im.show('road.jpg')
-elif current_weather >= 21 and current_weather <= 4:
-#night
+from datetime import time
+import time
+mytime = time.localtime()
+if mytime.tm_hour < 6 or mytime.tm_hour > 18:
     cv2.N = cv2.COLORMAP_OCEAN
     image = cv2.imread('road.jpg', cv2.IMREAD_COLOR)
     image_color = cv2.applyColorMap(image, cv2.N)
 
     cv2.imshow("Weather", image_color)
     cv2.waitKey()
-elif current_weather >= 17 and current_weather <= 20:
-#sunset
-    cv2.N = cv2.COLORMAP_HOT
-    image = cv2.imread('road.jpg')
-    image_color = cv2.applyColorMap(image, cv2.N)
-
-
-    cv2.imshow("Weather", image_color)
-    cv2.waitKey()
+else:
+    im = Image.open('road.jpg')
+    im.show()
