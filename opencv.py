cb04a9f729abe2fd5c1a9ba5d36aebc079a17d04
WilliamSampson44444 <42818523+WilliamSampson44444@users.noreply.github.com>
2020-05-10 11:13:16 -0700

    Updating opencv

--- opencv.py
+++ opencv.py
@@ -6,7 +6,7 @@ def background_filter(weather_type):
     now = datetime.now()
     image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
 
-    if((weather_type == "clouds") or ("rain" in weather_type) or (now.hour > 20 or now.hour < 5)):
+    if(("clouds" in weather_type) or ("rain" in weather_type) or (now.hour > 20 or now.hour < 5)):
         image = cv2.applyColorMap(image, cv2.COLORMAP_OCEAN)
     elif(("snow" in weather_type) or ("hail" in weather_type)):
         image = cv2.applyColorMap(image, cv2.COLORMAP_WINTER)
