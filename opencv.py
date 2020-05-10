54e820ac6ce300cf95e9b4dc705b4e20dc3ccb44
Nayeli De Jesus <nayelidjm142@gmail.com>
2020-05-09 18:29:00 -0700

    JS and PY added

--- opencv.py
+++ opencv.py
@@ -1,17 +1,25 @@
-from PIL import Image
+# from PIL import Image
+# import numpy as np
+# import cv2
+# import datetime
+# from datetime import time
+# import time
+# mytime = time.localtime()
+# if mytime.tm_hour < 6 or mytime.tm_hour > 18:
+# cv2.N = cv2.COLORMAP_OCEAN
+# image = cv2.imread('road1.jpg', cv2.IMREAD_COLOR)
+# image_color = cv2.applyColorMap(image, cv2.N)
+# filename = 'road2.jpg'
+# status = cv2.imwrite(filename, image_color)
+# else:
+#     im = Image.open('road.jpg')
+#     im = im.save("road3.jpg")
+
 import numpy as np
 import cv2
-import datetime
-from datetime import time
-import time
-mytime = time.localtime()
-if mytime.tm_hour < 6 or mytime.tm_hour > 18:
-    cv2.N = cv2.COLORMAP_OCEAN
-    image = cv2.imread('road.jpg', cv2.IMREAD_COLOR)
-    image_color = cv2.applyColorMap(image, cv2.N)
 
-    cv2.imshow("Weather", image_color)
-    cv2.waitKey()
-else:
-    im = Image.open('road.jpg')
-    im.show()
+cv2.N = cv2.COLORMAP_OCEAN
+image = cv2.imread('road1.jpg', cv2.IMREAD_COLOR)
+image_color = cv2.applyColorMap(image, cv2.N)
+filename = 'road2.jpg'
+status = cv2.imwrite(filename, image_color)
