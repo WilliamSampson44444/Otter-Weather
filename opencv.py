a79f2d5f05f75db699388eb72e429aec4e6a912a
Nayeli De Jesus <nayelidjm142@gmail.com>
2020-05-10 19:37:09 -0700

    some changes

--- opencv.py
+++ opencv.py
@@ -1,21 +1,23 @@
 import numpy as np
 import cv2
 from datetime import datetime
-from Tkinter import *
-import tkMessageBox
-import tkFont
-from PIL import ImageTk,Image
 
 def background_filter(weather_type):
     now = datetime.now()
     image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
 
     if(("clouds" in weather_type) or ("rain" in weather_type) or (now.hour > 20 or now.hour < 5)):
+        image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
         image = cv2.applyColorMap(image, cv2.COLORMAP_OCEAN)
+        status = cv2.imwrite('/static/Images/road.jpg', image)
     elif(("snow" in weather_type) or ("hail" in weather_type)):
+        image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
         image = cv2.applyColorMap(image, cv2.COLORMAP_WINTER)
+        status = cv2.imwrite('/static/Images/road.jpg', image)
     elif("fog" in weather_type):
+        image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
         image = cv2.applyColorMap(image, cv2.COLORMAP_BONE)
+        status = cv2.imwrite('/static/Images/road.jpg', image)
 
     status = cv2.imwrite('/static/Images/road.jpg', image)
 
