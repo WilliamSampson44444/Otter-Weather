8b933502ba37da5411d73c6dcc9a72bfef6d1a10
Nayeli De Jesus <nayelidjm142@gmail.com>
2020-05-10 19:44:36 -0700

    Test

--- opencv.py
+++ opencv.py
@@ -8,16 +8,16 @@ def background_filter(weather_type):
 
     if(("clouds" in weather_type) or ("rain" in weather_type) or (now.hour > 20 or now.hour < 5)):
         image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
-        image = cv2.applyColorMap(image, cv2.COLORMAP_OCEAN)
-        status = cv2.imwrite('/static/Images/road.jpg', image)
+        image_color = cv2.applyColorMap(image, cv2.COLORMAP_OCEAN)
+        status = cv2.imwrite('/static/Images/road.jpg', image_color)
     elif(("snow" in weather_type) or ("hail" in weather_type)):
         image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
-        image = cv2.applyColorMap(image, cv2.COLORMAP_WINTER)
-        status = cv2.imwrite('/static/Images/road.jpg', image)
+        image_color = cv2.applyColorMap(image, cv2.COLORMAP_WINTER)
+        status = cv2.imwrite('/static/Images/road.jpg', image_color)
     elif("fog" in weather_type):
         image = cv2.imread('static/Images/road1.jpg', cv2.IMREAD_COLOR)
-        image = cv2.applyColorMap(image, cv2.COLORMAP_BONE)
-        status = cv2.imwrite('/static/Images/road.jpg', image)
+        image_color = cv2.applyColorMap(image, cv2.COLORMAP_BONE)
+        status = cv2.imwrite('/static/Images/road.jpg', image_color)
 
     status = cv2.imwrite('/static/Images/road.jpg', image)
 
